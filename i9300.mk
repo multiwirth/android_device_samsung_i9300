ddbce3ea4a609976a5f51e052e87b67e523c4fb7
Shilin Victor <chrono.monochrome@gmail.com>
2020-03-07 18:24:36 +0300

    i9300: convert system.prop to vendor_prop.mk
    Change-Id: Id95b53a51c960787a415989021e932f1a1437b2e

--- i9300.mk
+++ i9300.mk
@@ -20,6 +20,9 @@ COMMON_PATH := device/samsung/smdk4412-common
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+# Vendor properties
+-include $(LOCAL_PATH)/vendor_prop.mk
+
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
